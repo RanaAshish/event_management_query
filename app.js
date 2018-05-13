7d822d5d9fae9a4805116429cf329a7825038b42
Ashish Rana <ashisharana@yahoo.com>
2018-05-13 11:24:20 +0530

    API prototype and package installation

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require("fs");
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -19,8 +19,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// read json data
+var data_obj = JSON.parse(fs.readFileSync('public/json/data.json', 'utf8'));
+
+console.log("Data = ",data_obj);
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,46 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/**
+ * To find session by session time
+ * /session_time/:session_time e.g. /session_time/morning will find morning session
+ * session_time can be from "morning" or "after-noon"
+ * Developed by "ar"
+ * API - 1
+ */
+router.get('/session_time/:session_time',async(req,res)=>{
+  res.status(200).json({"status":1,"message":"Under developement"});
+});
+
+/**
+ * To find next session
+ * /session/next/:type e.g. /session/next/session will find next session
+ * type can be from "session" or "break"
+ * Developed by "ar"
+ * API - 2/3
+ */
+router.get('/session/next/:type',async(req,res)=>{
+  res.status(200).json({"status":1,"message":"Under developement"});
+});
+
+/**
+ * To find session by speaker
+ * /speaker/:speaker_name e.g. /speaker/Ashish will find all session of Ashish
+ * Developed by "ar"
+ * API - 4
+ */
+router.get('/speaker/:speaker_name',async(req,res)=>{
+  res.status(200).json({"status":1,"message":"Under developement"});
+});
+
+/**
+ * To find session by name
+ * /session/:speaker_name e.g. /session/Welcome will find session having name "Welcome"
+ * Developed by "ar"
+ * API - 5
+ */
+router.get('/session/:session_name',async(req,res)=>{
+  res.status(200).json({"status":1,"message":"Under developement"});
+});
+
 module.exports = router;
